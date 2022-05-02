f04dde9665ddd16d302b63ab8bab6f1edad41e2f
Ankit <ankitup889@gmail.com>
2022-05-02 10:16:45 +0530

    AssingmentoneDone

--- ReactType/src/App.tsx
+++ ReactType/src/App.tsx
@@ -16,7 +16,7 @@
 // import Linkinf from "./Profj/Linkinf";
 // import Basi from "./FuncCompo/constexts/Basi";
 // store.subscribe(() => console.log(store.getState()));
-// import A from "./Task/A";
+// import A from "./Assingment1/A";
 
 const App = () => {
   return (
